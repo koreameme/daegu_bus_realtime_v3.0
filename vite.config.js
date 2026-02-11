6aff7d11b3fd88d41fec8e7bde2e0de684082d63
JHL <jhl@example.com>
2026-02-11 21:27:11 +0900

    Configure deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/daegu_bus_realtime/',
+    base: '/hanil-bus-daegu/',
     plugins: [
         react(),
         VitePWA({
